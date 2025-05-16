eae4ba54dc4d0138d133b93f661457100777b45d
Yashigami <kuzmin_var@bk.ru>
2025-05-16 19:43:49 +0300

    добавлены комментарии попытка увеличить RPS

--- internal/repository/wallet.go
+++ internal/repository/wallet.go
@@ -3,11 +3,9 @@ package repository
 import (
 	"fmt"
 	"github.com/Yashigami/wallet-service/internal/model"
-	"github.com/Yashigami/wallet-service/internal/utils"
 	"github.com/google/uuid"
 	"gorm.io/gorm"
 	"gorm.io/gorm/clause"
-	"time"
 )
 
 type WalletRepository interface {
@@ -23,44 +21,52 @@ func NewWalletRepository(db *gorm.DB) *WalletRepo {
 	return &WalletRepo{db: db}
 }
 
+// CreateOrUpdateWallet пополнение, снятие с кошелька
 func (r *WalletRepo) CreateOrUpdateWallet(op *model.WalletOperation) error {
-	const maxRetries = 5
-	for i := 0; i < maxRetries; i++ {
+	switch op.OperationType {
+	case model.Deposit:
+		// Пополнение - создать или увеличить баланс
 		err := r.db.Transaction(func(tx *gorm.DB) error {
-			var wallet model.Wallet
-			if err := tx.Clauses(clause.Locking{Strength: "UPDATE"}).FirstOrCreate(&wallet, model.Wallet{ID: op.WalletID}).Error; err != nil {
+			// Попытка создать кошелек, если его нет
+			err := tx.Clauses(clause.OnConflict{
+				Columns: []clause.Column{{Name: "id"}},
+				DoUpdates: clause.Assignments(map[string]interface{}{
+					"balance": gorm.Expr("wallets.balance + ?", op.Amount),
+				}),
+			}).Create(&model.Wallet{ID: op.WalletID, Balance: op.Amount}).Error
+			if err != nil {
 				return err
 			}
-			if op.OperationType == model.Withdraw && wallet.Balance < op.Amount {
-				return fmt.Errorf("insufficient funds")
-			}
-			if op.OperationType == model.Deposit {
-				wallet.Balance += op.Amount
-			} else if op.OperationType == model.Withdraw {
-				wallet.Balance -= op.Amount
-			} else {
-				return fmt.Errorf("invalid operation type")
-			}
-			if err := tx.Save(&wallet).Error; err != nil {
-				return err
-			}
-
 			return tx.Create(op).Error
 		})
-		if err == nil {
-			return nil
-		}
-		if utils.IsRetraybaleError(err) {
-			time.Sleep(time.Millisecond * 10)
-			continue
-		}
 		return err
+
+	case model.Withdraw:
+		// Снятие - атомарное обновление с проверкой баланса
+		err := r.db.Transaction(func(tx *gorm.DB) error {
+			res := tx.Exec(
+				`UPDATE wallets SET balance = balance - ? WHERE id = ? AND balance >= ?`,
+				op.Amount, op.WalletID, op.Amount,
+			)
+			if res.Error != nil {
+				return res.Error
+			}
+			if res.RowsAffected == 0 {
+				return fmt.Errorf("insufficient funds or wallet not found")
+			}
+			return tx.Create(op).Error
+		})
+		return err
+
+	default:
+		return fmt.Errorf("invalid operation type")
 	}
-	return fmt.Errorf("max retry attempts exceeded")
 }
 
+// GetBalance Баланс кошелька
 func (r *WalletRepo) GetBalance(walletID uuid.UUID) (int64, error) {
 	var wallet model.Wallet
+	// Получить кошелек
 	if err := r.db.First(&wallet, "id = ?", walletID).Error; err != nil {
 		return 0, err
 	}
